da60dc9d05e56794a7b20325c882ad0cfc3da8ca
Alex Elgin <sascha.elgin@gmail.com>
2019-05-24 13:03:28 +0600

    add Vuetify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false;
 
