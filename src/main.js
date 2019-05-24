afaa79215a33efd7092fc2558ba25ab448e3f271
Alex Elgin <sascha.elgin@gmail.com>
2019-05-24 17:00:24 +0600

    move router config to plugins

--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 
 import App from './App.vue';
-import router from './router';
 import store from './store';
 import i18n from './plugins/i18n';
+import router from './plugins/router';
 import './plugins/vee-validate';
 import './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  i18n,
   router,
   store,
-  i18n,
   render: h => h(App),
 }).$mount('#app');
