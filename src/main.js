0850ac8044650e504a2dd4b317519a95d89efe31
Alex Elgin <sascha.elgin@gmail.com>
2019-05-29 16:30:42 +0600

    move templates to plugins

--- src/main.js
+++ src/main.js
@@ -5,16 +5,11 @@ import App from './App.vue';
 import i18n from './plugins/i18n';
 import router from './plugins/router';
 import store from './plugins/store';
+import registerTemplates from './plugins/templates';
 import './plugins/vee-validate';
 import './plugins/vuetify';
 
-const templates = require.context('./templates', false, /\.vue$/);
-
-templates.keys().forEach((fileName) => {
-  const templateName = fileName.replace('./', '').replace('.vue', '');
-  Vue.component(`${templateName}-template`, templates(fileName).default);
-});
-
+registerTemplates(Vue);
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/plugins/templates.js
@@ -0,0 +1,10 @@
+const templates = require.context('../templates', false, /\.vue$/);
+
+export default function (Vue) {
+  templates.keys().forEach((fileName) => {
+    const templateName = fileName.replace('./', '').replace('.vue', '');
+    Vue.component(`${templateName}-template`, templates(fileName).default);
+  });
+
+  Vue.config.productionTip = false;// eslint-disable-line no-param-reassign
+}
