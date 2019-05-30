87195c77f91c411944ecf531edc35edc8da9f508
Alex Elgin <sascha.elgin@gmail.com>
2019-05-30 17:35:37 +0600

    accept both single-file based & folder-based template structures

--- src/plugins/templates.js
+++ src/plugins/templates.js
@@ -1,8 +1,8 @@
-const templates = require.context('../templates', false, /\.vue$/);
+const templates = require.context('../templates', true, /\.vue$/);
 
 export default function (Vue) {
   templates.keys().forEach((fileName) => {
-    const templateName = fileName.replace('./', '').replace('.vue', '');
+    const templateName = fileName.replace('./', '').replace('/index.vue', '').replace('.vue', '');
     Vue.component(`${templateName}-template`, templates(fileName).default);
   });
 }
