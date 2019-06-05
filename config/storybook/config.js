c98cf794fe9b5f690a0c35075ac96d9440e4249d
Alex Elgin <sascha.elgin@gmail.com>
2019-06-05 17:59:07 +0600

    integrate vuex into storybook

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -2,10 +2,12 @@
 import { configure, addDecorator } from '@storybook/vue';
 
 import './vuetify';
+import store from '@/plugins/store';
 import '@/plugins/vuetify';
 
 addDecorator(() => ({
   template: '<v-app><story/></v-app>',
+  store,
 }));
 
 const req = require.context('../../src', true, /.stories.js$/);
