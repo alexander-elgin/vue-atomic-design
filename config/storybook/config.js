d21501dee75ff53ea6c9f6c1be9863ad53b46df6
Alex Elgin <sascha.elgin@gmail.com>
2019-06-05 17:58:42 +0600

    integrate vuetify into storybook

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,5 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { configure } from '@storybook/vue';
+import { configure, addDecorator } from '@storybook/vue';
+
+import './vuetify';
+import '@/plugins/vuetify';
+
+addDecorator(() => ({
+  template: '<v-app><story/></v-app>',
+}));
 
 const req = require.context('../../src', true, /.stories.js$/);
 
