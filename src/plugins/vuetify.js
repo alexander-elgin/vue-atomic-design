6a4ab14834e189346c1c067bb7f299299f1be949
Alex Elgin <sascha.elgin@gmail.com>
2019-06-06 15:59:43 +0600

    change the vuetify palette

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,19 +1,15 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import 'vuetify/src/stylus/app.styl';
+import colors from 'vuetify/es5/util/colors';
 import ru from 'vuetify/es5/locale/ru';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#ee44aa',
-    secondary: '#424242',
-    accent: '#82B1FF',
-    error: '#FF5252',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FFC107',
+    primary: colors.green,
+    secondary: colors.red,
   },
   options: {
     customProperties: true,
