45e248ae897753e8eaf1c84ff204ed03b57867eb
Alex Elgin <sascha.elgin@gmail.com>
2020-01-06 17:59:44 +0600

    compile to ES5

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,16 @@
+/* eslint-disable indent */
+
 module.exports = {
   chainWebpack: (config) => {
+    const jsRule = config.module.rule('js');
+    jsRule.include.add(() => ['src']);
+
     config.module
       .rule('i18n')
-      .resourceQuery(/blockType=i18n/)
-      .type('javascript/auto')
-      .use('i18n')
-      .loader('@kazupon/vue-i18n-loader')
+        .resourceQuery(/blockType=i18n/)
+        .type('javascript/auto')
+        .use('i18n')
+        .loader('@kazupon/vue-i18n-loader')
       .end();
   },
   pluginOptions: {
