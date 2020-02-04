f07629e62d49fa4ca58b84589d0b332880e48103
Alex Elgin <sascha.elgin@gmail.com>
2020-02-04 20:02:32 +0600

    add a feature to cache GET requests

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-param-reassign */
 import axios from 'axios';
 import { isEmpty } from 'lodash';
+import LRU from 'receptacle';
 
 import { getToken, isAuthorized } from '@/utils/current-user';
 
@@ -23,6 +24,8 @@ const supplementHeaders = (config) => {
   return config;
 };
 
+const cache = new LRU({ max: 128 });
+
 const HTTP_METHODS_WITHOUT_DATA = ['delete', 'get'];
 const HTTP_METHODS_WITH_DATA = ['post', 'put'];
 
@@ -33,9 +36,21 @@ const originalMethods = HTTP_METHODS_WITHOUT_DATA.concat(HTTP_METHODS_WITH_DATA)
   }, {});
 
 HTTP_METHODS_WITHOUT_DATA.forEach((method) => {
-  axios[method] = (url, data = {}, config = {}) => {
+  axios[method] = (baseUrl, data = {}, config = {}) => {
     supplementHeaders(config);
-    return originalMethods[method](`${url}${isEmpty(data) ? '' : `?${serializeParams(data)}`}`, config);
+    const url = `${baseUrl}${isEmpty(data) ? '' : `?${serializeParams(data)}`}`;
+
+    if ((config.cache !== undefined) && cache.has(url)) {
+      return cache.get(url);
+    }
+
+    const result = originalMethods[method](url, config);
+
+    if (config.cache !== undefined) {
+      cache.set(url, result, { ttl: config.cache });
+    }
+
+    return result;
   };
 });
 
