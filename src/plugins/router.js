8ad0076b9676f7d8f5a4620e770fb16176bf1b07
Alex Elgin <sascha.elgin@fyve.com>
2021-09-02 18:49:03 +0600

    remove unused functionality

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '../pages/Home.vue';
+import Home from '../pages/home';
 
 Vue.use(Router);
 
@@ -13,15 +13,6 @@ export default new Router({
       name: 'home',
       component: Home,
       meta: { template: 'default' },
-    },
-    {
-      path: '/activities',
-      name: 'activities',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../pages/activities'),
-      meta: { template: 'lite' },
     },
   ],
 });
--- src/templates/default.vue
+++ src/templates/default.vue
@@ -1,17 +1,5 @@
 <template>
   <div>
-    <v-nav />
-    <img alt="Vue logo" src="../assets/logo.png">
     <slot />
   </div>
 </template>
-
-<script>
-import Nav from '@/organisms/nav';
-
-export default {
-  components: {
-    'v-nav': Nav,
-  },
-};
-</script>
