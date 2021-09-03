b1c50bb915151147b76609cf8c3a713ff039d1fa
Alex Elgin <sascha.elgin@fyve.com>
2021-09-03 14:03:50 +0600

    add date filter

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card max-width="480" min-width="480" class="mx-auto">
+  <v-card max-width="640" min-width="480" class="mx-auto">
     <v-toolbar color="primary" flat>
       <v-toolbar-title>{{ $t('lanck.pages.home.exchangeRates') }}</v-toolbar-title>
     </v-toolbar>
@@ -7,10 +7,8 @@
     <v-card-text>
       <v-container>
         <v-row no-gutters>
-          <v-col cols="6">
+          <v-col cols="5">
             <v-combobox
-              chips
-              clearable
               item-text="code"
               item-value="id"
               :items="currencies"
@@ -20,6 +18,36 @@
               return-object
               v-model="selectedCurrencies"
             ></v-combobox>
+          </v-col>
+          <v-col cols="2"></v-col>
+          <v-col cols="5">
+            <v-menu
+              v-model="datePickerShown"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              transition="scale-transition"
+              offset-y
+              min-width="auto"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  clearable
+                  @click:clear="selectedDate = undefined"
+                  :value="selectedDate | moment('DD.MM.YYYY')"
+                  :label="$t('lanck.pages.home.date')"
+                  prepend-icon="event"
+                  readonly
+                  v-bind="attrs"
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                color="primary"
+                first-day-of-week="1"
+                v-model="selectedDate"
+                @input="datePickerShown = false"
+              ></v-date-picker>
+            </v-menu>
           </v-col>
         </v-row>
         <v-row no-gutters>
@@ -70,7 +98,9 @@ export default {
   },
   data() {
     return {
+      datePickerShown: false,
       selectedCurrencies: [],
+      selectedDate: undefined,
     };
   },
   methods: {
@@ -78,8 +108,26 @@ export default {
       return this.currencies.find(({ id }) => id === currencyId).code;
     },
     getRates() {
-      return this.rates.filter(({ currency }) => this.selectedCurrencies.length === 0
-        || this.selectedCurrencies.map(({ id }) => id).includes(currency));
+      let filteredRates = this.rates
+        .filter(({ currency }) => this.selectedCurrencies.length === 0
+          || this.selectedCurrencies.map(({ id }) => id).includes(currency));
+
+      if (this.selectedDate === undefined) {
+        return filteredRates;
+      }
+
+      filteredRates = filteredRates.filter(({ date }) => this.selectedDate >= date);
+
+      const groupedRates = this.currencies.reduce((groupedValues, { id }) => Object.assign({
+        [id]: filteredRates.filter(({ currency }) => id === currency),
+      }, groupedValues), {});
+
+      Object.values(groupedRates).forEach(currencyRates => currencyRates
+        // eslint-disable-next-line no-confusing-arrow
+        .sort(({ date: dateA }, { date: dateB }) => dateA > dateB ? -1 : 1));
+
+      return Object.values(groupedRates).map(currencyRates => currencyRates[0])
+        .filter(rate => rate !== undefined);
     },
   },
 };
